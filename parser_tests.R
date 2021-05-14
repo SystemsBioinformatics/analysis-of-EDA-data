36da7c57dfb202761d05d1be5bb882e8ef65aeb6
douwe <d.molenaar@vu.nl>
2021-05-14 08:41:19 +0200

    adding new test data, testing single fluorimetric file

--- parser_tests.R
+++ parser_tests.R
@@ -1,14 +1,19 @@
 source("scripts/fractiomate_parser.R")
-folder <- "testdata/Antibiotics assay"
+folder1 <- "testdata/Antibiotics assay"
+folder2 <- "testdata/Sediment TTR"
 
-p1 <- parse_fractiomate(file.path(folder,"20201216_FractioMate_112957_EFF1 AB.txt"))
+p1 <- parse_fractiomate(file.path(folder1,"20201216_FractioMate_112957_EFF1 AB.txt"))
 
 source("scripts/platereader_parser.R")
 type_single_photometric_file <- "20201217_Kralingseveer fractionated_OD600_Plate 1.txt"
 type_multiple_fluorimetric_file <- "20201217_Kralingseveer fractionated_Resazurin_Plate 1.txt"
+type_single_fluorimetric_file <- "20200224 S4 TRIS.txt"
 
-file <- file.path(folder,type_single_photometric_file)
+file <- file.path(folder1,type_single_photometric_file)
 p2 <- parse_platereader(file)
 
-file <- file.path(folder,type_multiple_fluorimetric_file)
+file <- file.path(folder1,type_multiple_fluorimetric_file)
 p3 <- parse_platereader(file)
+
+file <- file.path(folder2,type_single_fluorimetric_file)
+p4 <- parse_platereader(file)
